0b289e40959ce83faa76f56d06663ca62802702b
Jakub Sitarek <01158893@pw.edu.pl>
2021-04-20 23:04:39 +0200

    changes to fit test2

--- main.py
+++ main.py
@@ -5,3 +5,24 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+@app.get("/method")
+def method_get():
+    return {"method": "GET"}
+
+@app.delete("/method")
+def method_delete():
+    return {"method": "DELETE"}
+
+@app.put("/method")
+def method_put():
+    return {"method": "PUT"}
+
+@app.options("/method")
+def method_options():
+    return {"method": "OPTIONS"}
+
+@app.post("/method", status_code=201)
+def method_put():
+    return {"method": "POST"}
+
